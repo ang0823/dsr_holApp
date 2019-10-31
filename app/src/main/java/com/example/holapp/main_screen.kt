b8fc8ff3c2b6adebe65420fd6d05a4acd458e8da
ang0823 <angelsanchez934@gmail.com>
2019-10-31 14:26:18 -0600

    Se agrego la pantalla de peril de usuario

---
+++ app/src/main/java/com/example/holapp/main_screen.kt
@@ -0,0 +1,19 @@
+package com.example.holapp
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main_screen.*
+
+class main_screen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_screen)
+        setSupportActionBar(toolbar)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/holapp/user_profile.kt
@@ -0,0 +1,12 @@
+package com.example.holapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class user_profile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile)
+    }
+}
